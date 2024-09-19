44389374d7d11c35ea3e0e96e72dcb4c5a14ce37
Daniel Alves <danielrosa4@edu.unisinos.br>
2024-09-18 21:55:06 -0300

    Add files via upload

---
+++ 3.5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+    int num;
+    char escolha[5];
+
+    
+
+    printf("Escolha par ou impar: ");
+    scanf("%s", &escolha);
+    escolha = toupper(escolha);
+    printf("Escolha um numero: ");
+    scanf("%d", &num);
+
+
+
+  
+    return 1;
+}
+
